75d47caece516362a3ba51addffeb3c124dc2f0b
wanggang <zhuzhouwg@163.com>
2020-12-27 21:00:28 +0800

    而此时

---
+++ template/src/main/java/com/myspringboot/template/code/rest/A.java
@@ -0,0 +1,12 @@
+package com.myspringboot.template.code.rest;
+
+/**
+ * @Description: java类作用描述
+ * @Package: com.myspringboot.template.code.rest
+ * @ClassName: A
+ * @Author: wanggang
+ * @CreateDate: 2020/12/27 20:51
+ * @Version: 1.0
+ */
+public class A {
+}
